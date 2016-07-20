d6c63b50a7e8671572cb1b94c951f099bbccb01f
wielki.borsuk <wielki.borsuk@gmail.com>
2016-07-20 14:05:43 +0200

    initial commit - proof of concept listener

---
+++ listen.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import tempfile
+
+while True:
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        # print(audio.sample_rate)
+        # print(audio.sample_width)
+
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as f:
+            f.write(audio.get_wav_data())
+
+        try:
+            print(r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("didn't hear anything familiar")
+            pass
+        except sr.RequestError:
+            print("couldn't reach the service")
+            pass
